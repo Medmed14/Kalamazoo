e3f87f1baac2be980edab19921b9c202709d0bcc
MehdiMemmi <mehdi@dsply.fr>
2021-12-08 17:31:50 +0100

    cards freelances in progress

--- src/pages/Freelances.jsx
+++ src/pages/Freelances.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import DefaultPicture from '../assets/profilPictures/profil.jpg'
 
 function Freelances() {
+
+    const freelanceProfiles = [
+        {
+            name: 'Jane Doe',
+            jobTitle: 'Devops',
+            picture: DefaultPicture,
+        },
+        {
+            name: 'John Doe',
+            jobTitle: 'Developpeur frontend',
+            picture: DefaultPicture,
+        },
+        {
+            name: 'Jeanne Biche',
+            jobTitle: 'Développeuse Fullstack',
+            picture: DefaultPicture,
+        },
+    ]
+
     return (
         <div>
             <h1>Freelances</h1>
