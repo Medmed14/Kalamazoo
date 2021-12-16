e0fdf0d040d4d2426d1a6aad5fed801e60ed74fc
MehdiMemmi <mehdi@dsply.fr>
2021-12-16 16:50:33 +0100

    page 404 ok

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import error from '../../assets/error/404.png'
+
+const ErrorWrapper = styled.div`
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.background};
+  align-items: center;
+`
+
+const ErrorTitle = styled.h1`
+  font-weight: 300;
+`
+
+const ErrorSubtitle = styled.h2`
+  font-weight: 300;
+  color: ${colors.secondary};
+`
+
+const Illustration = styled.img`
+  max-width: 800px;
+  border-radius: 5rem;
+`
 
 function Error() {
-    return (
-        <div>
-            <h1>ERROR 404 NOT FOUND!</h1>
-        </div>
-    )
+  return (
+    <ErrorWrapper>
+      <ErrorTitle>Oups...</ErrorTitle>
+      <Illustration src={error} />
+      <ErrorSubtitle>
+        Il semblerait que la page que vous cherchez n’existe pas
+      </ErrorSubtitle>
+    </ErrorWrapper>
+  )
 }
 
 export default Error
