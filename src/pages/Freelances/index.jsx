15d05d7800523b6b226200c8576fdaef32a2bc60
Mehdi Memmi <mehdi.memmi@laposte.net>
2022-01-13 15:10:49 +0100

    freelances list done

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,75 +1,87 @@
-import React from 'react'
-import DefaultPicture from '../../assets/profilPictures/profil.jpg'
-import Card from '../../components/Card/index'
+import { useEffect, useState } from 'react'
+import Card from '../../components/Card'
 import styled from 'styled-components'
-
+import colors from '../../utils/style/colors'
+import { Loader } from '../../utils/style/Atoms'
 
 const CardsContainer = styled.div`
-display: grid;
-padding-right: 10rem;
-padding-left: 10rem;
-gap: 24px;
-grid-template-rows: 350px 350px;
-grid-template-columns: repeat(3, 1fr);
-align-items: center;
-justify-items: center;
+  display: grid;
+  gap: 24px;
+  grid-template-rows: 350px 350px;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-items: center;
 `
-const TitleFreelances = styled.h1`
-    font-size: 1.875rem;
-    margin-top: 2.5rem;
-    color: #2F2E41;
-    font-weight: 600;
-    text-align: center;
+
+const PageTitle = styled.h1`
+  font-size: 30px;
+  color: black;
+  text-align: center;
+  padding-bottom: 30px;
 `
-const SubtitleFreelances = styled.h2`
-    margin-top: 3.5rem;
-    margin-bottom: 5.5rem;
-    font-size: 1.25rem;
-    font-weight: 600;
-    color: #8186A0;
-    text-align: center;
 
-
+const PageSubtitle = styled.h2`
+  font-size: 20px;
+  color: ${colors.secondary};
+  font-weight: 300;
+  text-align: center;
+  padding-bottom: 30px;
+`
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
 `
 
 function Freelances() {
+  const [isDataLoading, setDataLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [freelancersList, setFreelancesList] = useState([])
 
-    const freelanceProfiles = [
-        {
-            name: 'Jane Doe',
-            jobTitle: 'Devops',
-            picture: DefaultPicture,
-        },
-        {
-            name: 'John Doe',
-            jobTitle: 'Developpeur frontend',
-            picture: DefaultPicture,
-        },
-        {
-            name: 'Jeanne Biche',
-            jobTitle: 'Développeuse Fullstack',
-            picture: DefaultPicture,
-        },
-    ]
+  useEffect(() => {
+    async function fetchFreelances() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/freelances`)
+        const { freelancersList } = await response.json()
+        setFreelancesList(freelancersList)
+      } catch (err) {
+        console.log('===== error =====', err)
+        setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    fetchFreelances()
+  }, [])
 
-    
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
 
-    return (
-        <div>
-            <TitleFreelances>Trouvez votre prestataire idéal</TitleFreelances>
-            <SubtitleFreelances>Chez Kalamazoo nous réunissons les meilleurs profils pour vous.</SubtitleFreelances>
-            <CardsContainer>
-                {freelanceProfiles.map((profile, index) => (
-                    <Card
-                        key={`${profile.name}-${index}`}
-                        label={profile.jobTitle}
-                        title={profile.name}
-                    />
-                ))}
-            </CardsContainer>
-        </div>
-    )
+  return (
+    <div>
+      <PageTitle>Trouvez votre prestataire</PageTitle>
+      <PageSubtitle>
+        Chez Shiny nous réunissons les meilleurs profils pour vous.
+      </PageSubtitle>
+      {isDataLoading ? (
+        <LoaderWrapper>
+          <Loader />
+        </LoaderWrapper>
+      ) : (
+        <CardsContainer>
+          {freelancersList.map((profile, index) => (
+            <Card
+              key={`${profile.name}-${index}`}
+              label={profile.job}
+              title={profile.name}
+              picture={profile.picture}
+            />
+          ))}
+        </CardsContainer>
+      )}
+    </div>
+  )
 }
-
 
 export default Freelances
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
@@ -41,53 +40,45 @@ function Survey() {
   //pour le loader
   const [isDataLoading, setDataLoading] = useState(false)
   // pour error
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(false)
 
-//   useEffect(() => {
-//     setDataLoading(true)
-//     fetch(`http://localhost:8000/survey`)
-//     .then((response) => response.json())
-//     .then(({ surveyData }) => {
-//     setSurveyData(surveyData)
-//     setDataLoading(false)
-//     })
-// }, [])
 
-useEffect(() => {
-  async function fetchSurvey() {
-    setDataLoading(true)
-    try {
-      const response = await fetch(`http://localhost:8000/survey`)
-      const { surveyData } = await response.json()
-      setSurveyData(surveyData)
-     }
-    catch(err) {
-      setError(true)
+  useEffect(() => {
+    async function fetchSurvey() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:8000/survey`)
+        const { surveyData } = await response.json()
+        setSurveyData(surveyData)
+      }
+      catch (err) {
+        console.log(err)
+        setError(true)
+      }
+      finally {
+        setDataLoading(false)
+      }
     }
-    finally {
-      setDataLoading(false)
-    }
-  }
-  fetchSurvey()
-}, [] )
+    fetchSurvey()
+  }, [])
 
   return (
     <SurveyContainer>
-    <QuestionTitle>Question {questionNumber}</QuestionTitle>
-    {isDataLoading ? (
-      <Loader />
-    ) : (
-      <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
-    )}
-    <LinkWrapper>
-      <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-      {surveyData[questionNumberInt + 1] ? (
-        <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
       ) : (
-        <Link to="/results">Résultats</Link>
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
       )}
-    </LinkWrapper>
-  </SurveyContainer>
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
   )
 }
 
